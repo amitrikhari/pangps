a51f14525494aae354e467a6945e91da3c42b341
amitrikhari <75318246+amitrikhari@users.noreply.github.com>
2021-01-11 19:10:06 +0530

    Update vpnpack.sh

--- vpnpack.sh
+++ vpnpack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/amitrikhari/cv/raw/main/GlobalProtect_UI_deb-5.2.1.0-7.deb
+wget https://github.com/amitrikhari/pangps/raw/main/GlobalProtect_UI_deb-5.2.1.0-7.deb
 
 wget https://github.com/amitrikhari/pangps/raw/main/GlobalProtect_deb-5.2.1.0-7.deb
 
