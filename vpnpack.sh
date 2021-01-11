2d32cb806d3b48c38a667f05e0e7dbcbf7fd26c4
amitrikhari <75318246+amitrikhari@users.noreply.github.com>
2021-01-11 18:34:23 +0530

    Add files via upload

---
+++ vpnpack.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://github.com/amitrikhari/cv/raw/main/GlobalProtect_UI_deb-5.2.1.0-7.deb
+
+wget https://github.com/amitrikhari/pangps/raw/main/GlobalProtect_deb-5.2.1.0-7.deb
+
+#end
